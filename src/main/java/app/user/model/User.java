0ef0cbfa89561a7cbdcc0ae1efe240e1599c06ac
DTK - L <zapzapkiller@gmail.com>
2025-03-19 06:08:28 +0200

    Models for all entities are urgently needed to complete all the relations in time.
    Using JDK 23

--- src/main/java/app/user/model/User.java
+++ src/main/java/app/user/model/User.java
@@ -1,6 +1,70 @@
 package app.user.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
+import lombok.*;
+import org.hibernate.validator.constraints.UUID;
+
+import java.time.LocalDateTime;
+
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = false)
+    private String userhash;
+
+    @Column
+    private String shownUsername;
+
+    @Column
+    private String avatarUrl;
+
+    @Column
+    private String quote;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus userStatus;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserType userType;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column
+    private int totalPosts;
+
+    @Column
+    private LocalDateTime creationDate;
+
+    @Column
+    private LocalDateTime lastRedactDate;
+
+    @Column
+    private LocalDateTime lastActiveDate;
+
+
+    /*
+    * Here go the relations, i.e. LIsts for threads and posts
+    * */
+
 }
---
+++ src/main/java/app/user/model/UserStatus.java
@@ -0,0 +1,5 @@
+package app.user.model;
+
+public enum UserStatus {
+    ACTIVE, INACTIVE, BANNED
+}
